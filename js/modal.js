8e0b3287a380c40f670687d557ef17762f55d11e
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-07-02 11:44:03 -0700

    Release 1.1.2
    Clean up readme
    Add FAQ
    Update tested-up-tp
    Simplify which JS logic

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,8 @@
-    window._idl = {};
-    _idl.variant = "modal";
-    (function() {
-        var idl = document.createElement('script');
-        idl.type = 'text/javascript';
-        idl.async = true;
-        idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
-        document.getElementsByTagName('body')[0].appendChild(idl);
-    })();
+window._idl = {};
+	_idl.variant = "modal";
+	(function() {
+	    var idl = document.createElement('script');
+	    idl.async = true;
+	    idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
+	    document.getElementsByTagName('body')[0].appendChild(idl);
+	})();
