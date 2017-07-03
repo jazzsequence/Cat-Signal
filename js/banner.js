da0d1fbd7fee7b744edd8dc2fd7163460050ae79
Chris Reynolds <chris@jazzsequence.com>
2017-07-03 09:23:20 -0600

    remove conditional
    `_idl.variant` will never _not_ be set since it's defined on line 2, so just use what we've got.

--- js/banner.js
+++ js/banner.js
@@ -3,6 +3,6 @@ window._idl = {};
 	(function() {
 	    var idl = document.createElement('script');
 	    idl.async = true;
-	    idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
+	    idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant);
 	    document.getElementsByTagName('body')[0].appendChild(idl);
 	})();
